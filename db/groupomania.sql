53916ae437199c686808138ac9931a6e27b27732
Arditti Nathan <nath.arditti@yahoo.fr>
2022-05-15 22:45:19 +0200

    correction db

--- db/groupomania.sql
+++ db/groupomania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : Dim 15 mai 2022 à 20:40
+-- Généré le : Dim 15 mai 2022 à 20:44
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
